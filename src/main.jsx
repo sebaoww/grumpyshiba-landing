e31a2180c7b5e0b9e95d9547edcf761c0a67cf88
GrumpyShiba <sebaoww@gmail.com>
2025-04-04 16:25:56 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GrumpyShibaLanding from "./GrumpyShibaLanding";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GrumpyShibaLanding />
+  </React.StrictMode>
+);
